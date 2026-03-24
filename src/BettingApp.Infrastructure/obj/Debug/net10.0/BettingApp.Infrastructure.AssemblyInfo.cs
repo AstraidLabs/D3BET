5c33524d7140f26a75c6154e88f6171c4e625b35
Tomáš Vaněk <tomas.vanek@vancloud.cz>
2026-03-24 08:41:49 +0100

    add

--- src/BettingApp.Infrastructure/obj/Debug/net10.0/BettingApp.Infrastructure.AssemblyInfo.cs
+++ src/BettingApp.Infrastructure/obj/Debug/net10.0/BettingApp.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BettingApp.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bfb811ea7b003423c1840ec73308a8b88655d38")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45c60130ebe37448822e980fb5596b63015ecbb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("BettingApp.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BettingApp.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/BettingApp.Wpf/obj/Debug/net10.0-windows/BettingApp.Wpf.AssemblyInfo.cs
+++ src/BettingApp.Wpf/obj/Debug/net10.0-windows/BettingApp.Wpf.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BettingApp.Wpf")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+babe3062bd1d751e15ac1cc5d703ad4a23d2c8a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45c60130ebe37448822e980fb5596b63015ecbb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("BettingApp.Wpf")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BettingApp.Wpf")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
